9572d504d9135a0e001557e84638e93278e6a5a8
Renleilei_Johnny <renleilei1992@foxmail.com>
2017-11-09 16:21:06 +0800

    013_iterator.cpp updating! :)

--- Main/013_iterator.cpp
+++ Main/013_iterator.cpp
@@ -10,9 +10,11 @@
 ================================================================*/
 
 #include <vector>
+#include <map>
 #include <iostream>
 #include <algorithm>
 #include <functional>
+#include <string>
 
 using namespace std;
 
@@ -25,10 +27,12 @@ int main()
 	int ia[] = {1, 2, 3};
 	cout<<"sizeof(ia)/sizeof(int) = ["<<sizeof(ia)/sizeof(int)<<"]"<<endl;
 	vector<int> ivec(ia, ia + sizeof(ia)/sizeof(int));
-	
 	/* vector的第二种初始化方法 */
 	vector<int> _vec02 = {10, 11, 12, 13, 14, 15, 16};
+	/* map容器的初始化方法 */
+	map<int, string> imapStudent;
 
+	/* vector 遍历*/
 	/* 1.使用迭代器遍历容器 */
 	for(vector<int>::const_iterator iter = ivec.begin(); iter != ivec.end(); ++iter){
 		cout<< *iter << endl;
@@ -40,11 +44,40 @@ int main()
 	for_each(ivec.begin(), ivec.end(), [ &](int _A){ _A = _A + 1; cout<<_A<<","<<endl;});		//output: 2, 3, 4
 	/* 4.for loop auto 区间遍历 */
 	for(auto iter = _vec02.begin(); iter != _vec02.end(); iter++){		//基础型迭代器与auto遍历
-		cout<< (*iter) <<endl;
+		cout<< (*iter) <<" ";
 	}
-	for(auto val : _vec02){
-		cout<< val <<endl;
+	cout<<endl;
+	for(auto val : _vec02){		//区间遍历
+		cout<< val <<" ";
 	}
+	cout<<endl;
+	
+	/* map 遍历 */
+	imapStudent.insert(pair<int, string>(1, "student 1"));				//插入方式 - 1
+	imapStudent.insert(map<int, string>::value_type (2, "student 2"));	//插入方式 - 2 [与方式1其实类似]
+	imapStudent[3] = "student 3";										//插入方式 - 3 [数组方式插入]
+	cout<<"map.size: "<<imapStudent.size()<<endl;						//获取map容器已存入的数据数量
+	/* 1.迭代器for loop普通遍历 */
+	map<int, string>::iterator iter_map;								//设置前向迭代器
+	for(iter_map = imapStudent.begin(); iter_map != imapStudent.end(); ++iter_map){			//应用前向迭代器
+		cout<<iter_map->first<<"-"<<iter_map->second<<"   ";
+	}
+	cout<<endl;
+	map<int, string>::reverse_iterator iter_map2;						//设置反向迭代器
+	for(iter_map2 = imapStudent.rbegin(); iter_map2 != imapStudent.rend(); ++iter_map2){		//应用反向迭代器
+		cout<<iter_map2->first<<"-"<<iter_map2->second<<"   ";
+	}
+	cout<<endl;
+	/* 2.数组形式遍历 */
+	for(int i = 1; i <= (int )imapStudent.size(); ++i){
+		cout<<imapStudent[i]<<"   ";
+	}
+	cout<<endl;
+	/* 3.for loop auto 区间遍历 */
+	for(auto &val_map : imapStudent){
+		cout<<val_map.first<<"-"<<val_map.second<<"   ";
+	}
+	cout<<endl;
 }
 
 // for_each() 事实上是一个function template, 实现如下
