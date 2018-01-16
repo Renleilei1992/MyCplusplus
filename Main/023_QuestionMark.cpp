1f3510df571df02b76d535c0ab438995e48b4157
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-01-16 23:51:55 +0800

    023.cpp about QuestionMark

---
+++ Main/023_QuestionMark.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+enum state
+{
+	eTgroup_01,
+	eTgroup_02,
+};
+
+int main(int argc, char **argv)
+{
+	state m_eTgroupState = eTgroup_01;
+
+	int num_01 = (m_eTgroupState == eTgroup_02) ? 1 : 2;
+
+	cout<<num_01<<endl;
+
+	if(!(m_eTgroupState == eTgroup_01))
+	{
+		cout<<"It's equal!"<<endl;
+	}
+	return 0;
+}
