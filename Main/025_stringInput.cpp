cad71ee696b8b0ca2c9bed3e247836f54b18e4ff
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-01-29 21:14:06 +0800

    025.cpp commit!

---
+++ Main/025_stringInput.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	string strInput = "";
+	cout<<"plz input: ";
+	cin>>strInput;
+
+	int length = strInput.length();
+	cout<<strInput<<endl;
+	
+	char chInput[length] = {};
+	for(int i = 0; i < length; ++i)
+	{
+		if(strInput[i])
+	}
+
+	return 0;
+}
