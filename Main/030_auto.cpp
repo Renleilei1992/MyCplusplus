15523cec713aa8ef958877f52ef759ca26154c4b
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-02-06 20:38:21 +0800

    all commit!

---
+++ Main/030_auto.cpp
@@ -0,0 +1,54 @@
+/*================================================================
+*   Copyright (C) 2018 Renleilei. All rights reserved.
+*   
+*   文件名称：030_auto.cpp
+*   创 建 者：Renleilei (renleilei@foxmail.com)
+*   创建日期：2018年02月02日
+*   描    述：
+*   版    本: Version 1.00
+*	编译方法: g++ -o main 030_auto.cpp -std=c++11 -g -Wall -O3
+================================================================*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void myPrint(int i);
+
+int main()
+{
+	std::vector<int> vec_01;
+	vec_01.push_back(1);
+	vec_01.push_back(2);
+	vec_01.push_back(3);
+	vec_01.push_back(4);
+
+	// use normal iterator
+	cout << "With iterator: " << endl;
+	std::vector<int>::iterator it = vec_01.begin();
+	for(it; it != vec_01.end(); ++it){ cout << *it << endl; }
+
+	// use auto key word
+	cout << "With normal auto iterator: " << endl;
+	for(auto ivec_01 = vec_01.begin(); ivec_01 != vec_01.end(); ++ivec_01){ cout << *ivec_01 <<endl; }
+
+	// use auto key word with range
+	cout << "With auto range: " << endl;
+	for(auto  ivec : vec_01){ cout << ivec << endl; }
+
+	// use for_each key word
+	cout << "With Normal for_each: " << endl;
+	for_each(vec_01.begin(), vec_01.end(), myPrint);
+
+	// use lamanda and for_each
+	cout << "With Lamanda express: " << endl;
+	for_each(vec_01.begin(), vec_01.end(), [](int i){ cout << i << endl;});
+	return 0;
+}
+
+void myPrint(int i)
+{
+	cout << i << endl;
+}
