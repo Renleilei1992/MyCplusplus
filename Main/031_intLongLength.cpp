04859a81abc6ffd53c59ed07d46eaf8736f80b95
renleilei_mac <renleilei@hujiang.com>
2018-02-26 20:14:49 +0800

    031.cpp commit about the length of long

---
+++ Main/031_intLongLength.cpp
@@ -0,0 +1,21 @@
+/*================================================================
+*   Copyright (C) 2018 Renleilei. All rights reserved.
+*   
+*   文件名称：031_intLongLength.cpp
+*   创 建 者：Renleilei (renleilei@foxmail.com)
+*   创建日期：2018年02月26日
+*   描    述：
+*   版    本: Version 1.00
+*   编译命令: 
+================================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long num;
+	cout<<sizeof(num)<<endl;
+	return 0;
+}
