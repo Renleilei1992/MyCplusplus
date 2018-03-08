a0eb77705b2a54abf466b0d6a611d308b54182bb
renleilei_mac <renleilei@hujiang.com>
2018-03-08 10:26:12 +0800

    033_ManyParameter.cpp commit

---
+++ Main/033_ManyParamter.cpp
@@ -0,0 +1,34 @@
+/*================================================================
+*   Copyright (C) 2018 Renleilei. All rights reserved.
+*   
+*   文件名称：033_ManyParamter.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2018年03月08日
+*   描    述：
+*   版    本: Version 1.00
+*   编译命令: 
+================================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+#define MAX_COUNT 1000
+
+void func_01(int a1, int a2 = MAX_COUNT, int a3 = 100, int a4 = 10);
+
+int main()
+{
+	func_01(10);
+	func_01(10,10000);
+
+	return 0;
+}
+
+void func_01(int a1, int a2, int a3, int a4)
+{
+	cout<<"a1 = "<<a1<<endl;
+	cout<<"a2 = "<<a2<<endl;
+	cout<<"a3 = "<<a3<<endl;
+	cout<<"a4 = "<<a4<<endl;
+}
