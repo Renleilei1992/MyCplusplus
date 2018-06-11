47baca1489efd72369f799214eccc0e64a9dc4bf
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-06-11 14:36:06 +0800

    some cMake file commit!

---
+++ CMake/CMakeFiles/Main_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main_1.dir/002_Milo_ChristmasTree.cpp.o"
+  "Main_1.pdb"
+  "Main_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
