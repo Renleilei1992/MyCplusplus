cfce7123585c1d29822479e07df5859eb4f60221
Johnny_Aliyun_New <renleilei1992@foxmail.com>
2018-09-20 10:59:48 +0800

    073.cpp about and-Symbol

---
+++ Main/073_and.cpp
@@ -0,0 +1,25 @@
+/*================================================================
+*   Copyright (C) 2018 Renleilei. All rights reserved.
+*   
+*   文件名称：073_and.cpp
+*   创 建 者：Renleilei (renleilei@foxmail.com)
+*   创建日期：2018年08月30日
+*   描    述：
+*   版    本: Version 1.00
+================================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	if (2 & 2) {
+		cout << "yes" << endl;
+	}
+	if (2 & 3) {
+		cout << "yes" << endl;
+	}
+
+	return 0;
+}
