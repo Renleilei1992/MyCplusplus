12244efb1a00d0a1bf5e8ec9371913476022e57c
Johnny_Aliyun_New <renleilei1992@foxmail.com>
2019-01-10 16:43:31 +0800

    079.cpp about boolean value commit!

---
+++ Main/079_boolValue.cpp
@@ -0,0 +1,26 @@
+/*================================================================
+*   Copyright (C) 2018 Renleilei. All rights reserved.
+*   
+*   文件名称：079_boolValue.cpp
+*   创 建 者：Renleilei (renleilei@foxmail.com)
+*   创建日期：2018年11月07日
+*   描    述：
+*   版    本: Version 1.00
+================================================================*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+
+	bool ret_val = true;
+
+	cout << ret_val << endl;
+
+	printf("======>  %d\n", ret_val);
+
+	return 0;
+}
