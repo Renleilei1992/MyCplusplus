e47f0bfbb051b0bb8447f4d0933c975c05751bbf
Johnny_Aliyun_New <renleilei1992@foxmail.com>
2019-03-25 16:39:42 +0800

    090.cpp about empty class size commit

---
+++ Main/090_emptyClassSize.cpp
@@ -0,0 +1,26 @@
+/*================================================================
+*   Copyright (C) 2019 Renleilei. All rights reserved.
+*   
+*   文件名称：090_emptyClassSize.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2019年01月24日
+*   描    述：
+*   版    本: Version 1.00
+================================================================*/
+
+new line
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+};
+
+int main()
+{
+	cout << "sizeof(A) = " << sizeof(A) << endl;
+	
+	return 0;
+}
+new line
