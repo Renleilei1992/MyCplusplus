3e7ec4c1e33cbab5646f7e1e3fcfdd1fdd9b99a8
newAliyun_Johnny <renleilei1992@foxmail.com>
2019-05-06 09:51:27 +0800

    112.cpp about const用法提交

---
+++ Main/112_const_using.cpp
@@ -0,0 +1,57 @@
+/*================================================================
+*   Copyright (C) 2019 Renleilei. All rights reserved.
+*   
+*   文件名称：112_const_using.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2019年05月04日
+*   描    述：const的各类使用场景及用法
+*   版    本: Version 1.00
+================================================================*/
+
+// 类
+class A
+{
+private:
+    const int a;                // 常对象成员，只能在初始化列表赋值
+
+public:
+    // 构造函数
+    A() : a(0) { };
+    A(int x) : a(x) { };        // 初始化列表
+
+    // const可用于对重载函数的区分
+    int getValue();             // 普通成员函数
+    int getValue() const;       // 常成员函数，不得修改类中的任何数据成员的值
+};
+
+void function()
+{
+    // 对象
+    A b;                        // 普通对象，可以调用全部成员函数
+    const A a;                  // 常对象，只能调用常成员函数、更新常成员变量
+    const A *p = &a;            // 常指针
+    const A &q = a;             // 常引用
+
+    // 指针
+    char greeting[] = "Hello";
+    char* p1 = greeting;                // 指针变量，指向字符数组变量
+    const char* p2 = greeting;          // 指针变量，指向字符数组常量
+    char* const p3 = greeting;          // 常指针，指向字符数组变量
+    const char* const p4 = greeting;    // 常指针，指向字符数组常量
+}
+
+// 函数
+void function1(const int Var);           // 传递过来的参数在函数内不可变
+void function2(const char* Var);         // 参数指针所指内容为常量
+void function3(char* const Var);         // 参数指针为常指针
+void function4(const int& Var);          // 引用参数在函数内为常量
+
+// 函数返回值
+const int function5();      // 返回一个常数
+const int* function6();     // 返回一个指向常量的指针变量，使用：const int *p = function6();
+int* const function7();     // 返回一个指向变量的常指针，使用：int* const p = function7();
+
+int main()
+{
+	return 0;
+}
