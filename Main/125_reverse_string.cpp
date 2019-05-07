a5d796012185a46ef36ac89b5fbc8f50d3b5aec0
newAliyun_Johnny <renleilei1992@foxmail.com>
2019-05-07 23:36:14 +0800

    125.cpp about 反转字符串、反转单词提交

---
+++ Main/125_reverse_string.cpp
@@ -0,0 +1,71 @@
+/*================================================================
+*   Copyright (C) 2019 Renleilei. All rights reserved.
+*   
+*   文件名称：125_reverse_string.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2019年05月07日
+*   描    述：
+*   版    本: Version 1.00
+================================================================*/
+
+#include <iostream>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+void reverseStr(const string& str);
+void reverseWord(const string& str);
+
+int main()
+{
+	string str1 = "Hello World!";
+
+	reverseStr(str1);
+	reverseWord(str1);
+
+	return 0;
+}
+
+// 反转字符串,使用栈的特性
+void reverseStr(const string& str)
+{
+	stack<char> strStack;
+	for(int i = 0; i < str.length(); ++i) {
+		cout << "str[" << i << "]: " << str[i] << endl;
+		strStack.push(str[i]);
+	}
+
+	cout << "----- 翻转后的字符串为: ";
+	while(!strStack.empty()) {
+		cout << strStack.top();
+		strStack.pop();
+	}
+	cout << endl;
+}
+
+// 反转单词
+void reverseWord(const string& str)
+{
+	stack<string> strStack;
+	string temp;
+	for(int i = 0; i < str.length(); ++i) {
+		if(str[i] == ' ') {
+			strStack.push(temp);
+			temp = "";
+			strStack.push(string(" "));
+		} else {
+			temp += str[i];
+			if(i == str.length() - 1) {
+				strStack.push(temp);
+				temp = "";
+			}
+		}
+	}
+
+	while(!strStack.empty()) {
+		cout << strStack.top();
+		strStack.pop();
+	}
+	cout << endl;
+}
