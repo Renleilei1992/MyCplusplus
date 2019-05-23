dfa0f4aed0f925a396d46ee9f87d070613d14d44
newAliyun_Johnny <renleilei1992@foxmail.com>
2019-05-23 14:59:26 +0800

    131.cpp about 打印普通数组提交，未完成待续

---
+++ Main/131_print_allArray.cpp
@@ -0,0 +1,36 @@
+/*================================================================
+*   Copyright (C) 2019 Renleilei. All rights reserved.
+*   
+*   文件名称：131_print_allArray.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2019年05月09日
+*   描    述：打印全排列
+*   版    本: Version 1.00
+================================================================*/
+
+// 题目原始条件: 给定数组 {1, 2, 3}
+// 打印出如下结果:
+// 1 2 3
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
+
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int len);
+
+int main()
+{
+	int arr[3] = {1, 2, 3};
+
+	return 0;
+}
+
+void printArray(int arr[], int len)
+{
+	cout << "---- printArray begin ----\n";
+}
