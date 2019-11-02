c3ceefd32a4c2f97ea986ee723311e42034c436c
renleilei_DesktopWin10PC <renleilei1992@foxmail.com>
2019-11-02 22:26:14 +0800

    modify: 209.cpp 更改观察者存储方式为list

--- Main/209_for_interview_observer.cpp
+++ Main/209_for_interview_observer.cpp
@@ -1,6 +1,6 @@
 /*================================================================
 *   Copyright (C) 2019 Renleilei. All rights reserved.
-*   
+*
 *   文件名称：209_for_interview_observer.cpp
 *   创 建 者：Renleilei (renleilei1992@foxmail.com)
 *   创建日期：2019年11月02日
@@ -9,7 +9,7 @@
 ================================================================*/
 
 #include <iostream>
-#include <vector>
+#include <list>
 using namespace std;
 
 class IMessageObserver
@@ -22,32 +22,32 @@ public:
 class IMessage
 {
 public:
-	int RegisterMessageObserver(IMessageObserver* observer) { 
-		if(observer != nullptr) {
+	int RegisterMessageObserver(IMessageObserver* observer) {
+		if (observer != nullptr) {
 			cout << __func__ << endl;
-			m_vecMsgObservers.emplace_back(observer);
+			m_listMsgObservers.emplace_back(observer);
 		}
+
+		return 0;
 	}
 
 	int DeRegisterMessageObserver(IMessageObserver* observer) {
-		for (std::vector<IMessageObserver*>::iterator it = m_vecMsgObservers.begin(); it != m_vecMsgObservers.end(); ++it) {
-	//		if (it == observer) {
-				cout << __func__ << endl;
-				m_vecMsgObservers.erase(it);
-	//		}
-		}
+		cout << __func__ << endl;
+		m_listMsgObservers.remove(observer);
+
+		return 0;
 	}
 
 	void notify(int a, int b, const char* lpStr) {
 		cout << "IMessage::" << __func__ << " a=" << a << " b=" << b << " lpStr: " << lpStr << endl;
-		for (auto iVec : m_vecMsgObservers) {
-			iVec->onRsp(a, b, lpStr);
-			iVec->onNtf(a, b, lpStr);
+		for (auto iList : m_listMsgObservers) {
+			iList->onRsp(a, b, lpStr);
+			iList->onNtf(a, b, lpStr);
 		}
 	}
 
 private:
-	std::vector<IMessageObserver*> m_vecMsgObservers;
+	std::list<IMessageObserver*> m_listMsgObservers;
 };
 
 static IMessage* s_Message_ptr = new IMessage();
@@ -65,7 +65,7 @@ public:
 		s_Message_ptr->DeRegisterMessageObserver(this);
 	}
 
-	void show() { cout << "A::" << __func__ << " hello A" <<endl; }
+	void show() { cout << "A::" << __func__ << " hello A" << endl; }
 
 	virtual void onRsp(int a, int b, const char* lpStr) {
 		cout << "A::" << __func__ << " a=" << a << " b=" << b << " lpStr: " << lpStr << endl;
@@ -75,14 +75,42 @@ public:
 	}
 };
 
+class B : public IMessageObserver
+{
+public:
+	B() {
+		cout << __func__ << endl;
+		s_Message_ptr->RegisterMessageObserver(this);
+	}
+
+	~B() {
+		cout << __func__ << endl;
+		s_Message_ptr->DeRegisterMessageObserver(this);
+	}
+
+	void show() { cout << "B::" << __func__ << " hello B" << endl; }
+
+	virtual void onRsp(int a, int b, const char* lpStr) {
+		cout << "B::" << __func__ << " a=" << a << " b=" << b << " lpStr: " << lpStr << endl;
+	}
+	virtual void onNtf(int a, int b, const char* lpStr) {
+		cout << "B::" << __func__ << " a=" << a << " b=" << b << " lpStr: " << lpStr << endl;
+	}
+};
+
 int main()
 {
 	A* a_ptr = new A();
 	a_ptr->show();
+	B* b_ptr = new B();
+	b_ptr->show();
 
 	// 遍历观察者通知
 	s_Message_ptr->notify(5, 10, "hello observers!!");
 
 	delete a_ptr;
+	delete b_ptr;
+	delete s_Message_ptr;
 	return 0;
 }
+
