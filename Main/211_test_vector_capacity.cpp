b9b45aed09860feefceea43df8075f12684c6580
newAliyun_Johnny <renleilei1992@foxmail.com>
2019-11-08 14:14:18 +0800

    211.cpp 提交修改: 添加资源释放及vector存储智能指针机制

--- Main/211_test_vector_capacity.cpp
+++ Main/211_test_vector_capacity.cpp
@@ -6,6 +6,7 @@
 *   创建日期：2019年11月02日
 *   描    述：简单测试vector自动扩容时存储的指针和对象的区别
 *   版    本: Version 1.00
+*   编译指令: g++ -o main 211_test_vector_capacity.cpp -std=c++14
 ================================================================*/
 
 /*
@@ -14,6 +15,7 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>	// std::unique_ptr std::make_unique
 
 using namespace std;
 
@@ -32,8 +34,6 @@ int main()
 	A* a1 = new A();
 	A* a2 = new A();
 	A* a3 = new A();
-	A* a4 = new A();
-	A* a5 = new A();
 
 	cout << "-----------------------ptr begin push_back --------------------------" << endl;
 	vec_1.push_back(a1);
@@ -57,5 +57,23 @@ int main()
 	cout << "capacity: " << vec_2.capacity() << endl;
 	cout << "-----------------------end begin push_back --------------------------" << endl;
 
+	std::vector<std::unique_ptr<A>> vec_3;
+	cout << "-----------------------unique_ptr begin emplace_back --------------------------" << endl;
+	vec_3.emplace_back(std::make_unique<A>());
+	cout << "capacity: " << vec_3.capacity() << endl;
+	vec_3.emplace_back(std::make_unique<A>());
+	cout << "capacity: " << vec_3.capacity() << endl;
+	vec_3.emplace_back(std::make_unique<A>());
+	cout << "capacity: " << vec_3.capacity() << endl;
+	cout << "-----------------------unique_ptr end emplace_back --------------------------" << endl;
+
+
+	cout << "---------------> delete vec_1 begin" << endl;
+	for (auto iVec : vec_1) {
+		if (iVec != nullptr) {
+			delete iVec;
+		}
+	}
+	cout << "---------------> delete vec_1 end" << endl;
 	return 0;
 }
