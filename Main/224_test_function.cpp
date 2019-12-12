ba8d0db36bac3e717be47b13600584b8b64f5117
newAliyun_Johnny <renleilei1992@foxmail.com>
2019-12-12 18:00:41 +0800

    224.cpp about c++11 function函数模板的简单使用

---
+++ Main/224_test_function.cpp
@@ -0,0 +1,60 @@
+/*================================================================
+*   Copyright (C) 2019 Renleilei. All rights reserved.
+*   
+*   文件名称：224_test_function.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2019年12月12日
+*   描    述：简单测试C++11的 function 类模板
+*   版    本: Version 1.00
+================================================================*/
+
+#include <iostream>
+#include <map>
+#include <functional>		// std::function
+
+using namespace std;
+using namespace std::placeholders;		// _1 _2
+
+// 加法函数
+int add(int op1, int op2) {
+	return op1 + op2;
+}
+
+// 减法 lambda表达式
+auto minus_op = [](int op1, int op2) { return op1 - op2; };
+
+// 函数对象
+struct multiply_op {
+	int operator()(int op1, int op2) {
+		return op1 * op2;
+	}
+};
+
+// 除法函数
+int devide(int op1, int op2, bool check0) {
+	if (check0 && (op2 == 0)) {
+		return 0;
+	}
+
+	return op1 / op2;
+}
+
+// 参数绑定
+auto dev_op = bind(devide, _1, _2, false);
+
+int main()
+{
+	std::map<string,std::function<int(int, int)>> op = {
+		{"+", add},
+		{"-", minus_op},
+		{"*", multiply_op()},
+		{"/", dev_op},
+	};
+
+	cout << "测试加法: " << op["+"](1, 2) << endl;
+	cout << "测试减法: " << op["-"](3, 4) << endl;
+	cout << "测试乘法: " << op["*"](5, 6) << endl;
+	cout << "测试除法: " << op["/"](7, 8) << endl;
+
+	return 0;
+}
