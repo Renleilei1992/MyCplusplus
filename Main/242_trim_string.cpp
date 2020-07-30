1fcccad34d8ee7e094e9d992e519bbf460b90634
newAliyun_Johnny <renleilei1992@foxmail.com>
2020-07-30 09:53:31 +0800

    242.cpp about 清理字符串stdstring空格的几种方式

---
+++ Main/242_trim_string.cpp
@@ -0,0 +1,52 @@
+/*================================================================
+*   Copyright (C) 2020 Renleilei. All rights reserved.
+*   
+*   文件名称：242_trim_string.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2020年07月30日
+*   描    述：trim std::string的几种方式
+*   版    本: Version 1.00
+*   参考文献: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+================================================================*/
+
+#include <algorithm> 
+#include <cctype>
+#include <locale>
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
+    return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+}
