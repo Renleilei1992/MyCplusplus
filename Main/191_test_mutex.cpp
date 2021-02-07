8b810fe344eb44d42eaf9153c557f9cc7ad293ba
newAliyun_Johnny <renleilei1992@foxmail.com>
2021-02-07 14:18:21 +0800

    update 191_test_mutex.cpp about 修改一些注释以及线程的补充

--- Main/191_test_mutex.cpp
+++ Main/191_test_mutex.cpp
@@ -34,9 +34,9 @@ int main()
 void func(int a)
 {
 	{
-		cout << __func__ << "::waiting for the mutex!" << endl;
+		cout << a << __func__ << "::waiting for the mutex!" << endl;
 		std::lock_guard<std::mutex> lck(mtx);	// 进入作用域, 构造函数获得mtx锁
-		cout << __func__ << "::get the mutex! thread func be called! a: " << a << " thread id: " << std::this_thread::get_id() << endl;
+		cout << a << __func__ << "::get the mutex! thread func be called! a: " << a << " thread id: " << std::this_thread::get_id() << endl;
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 	}
 	// 离开作用域，析构函数会释放mtx锁
@@ -47,8 +47,10 @@ void testThreadFunc()
 	std::vector<std::thread> threadVec;
 	{
 		std::lock_guard<std::mutex> lck(mtx);	// 进入作用域，构造函数获取mtx锁
-		std::thread t1(func, 10);				// 创建线程t1	// 线程对象没办法直接push_back进容器;	需要使用std::move
+		std::thread t1(func, 1);				// 创建线程t1	// 线程对象没办法直接push_back进容器;	需要使用std::move
+		std::thread t2(func, 2);				// 创建线程t2	// 线程对象没办法直接push_back进容器;	需要使用std::move
 		threadVec.emplace_back(std::move(t1));	// 推荐使用此方式存储到容器中, emplace_back可以减少push_back的额外复制和移动操作
+		threadVec.emplace_back(std::move(t2));
 //		t1.detach();
 		cout << __func__ << " main thread will release the mutex rightnow! main thread id: " << std::this_thread::get_id() << endl;
 		std::this_thread::sleep_for(std::chrono::seconds(1));
