db787cf077d1eb121d8bae3fb8b7c3d9309dca10
newAliyun_Johnny <renleilei1992@foxmail.com>
2021-03-28 17:45:07 +0800

    245.cpp about 空实现

---
+++ Main/245_test_thread.cpp
@@ -0,0 +1,17 @@
+/*================================================================
+*   Copyright (C) 2020 Renleilei. All rights reserved.
+*   
+*   文件名称：245_test_thread.cpp
+*   创 建 者：Renleilei (renleilei1992@foxmail.com)
+*   创建日期：2020年09月19日
+*   描    述：
+*   版    本: Version 1.00
+================================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
